c17503aa562c077f28e532f8c8c77d0abf46ece9
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-24 10:59:36 +0300

    added AuthResponseBodyModel.java

---
+++ src/test/java/models/AuthResponseBodyModel.java
@@ -0,0 +1,16 @@
+package models;
+import lombok.Data;
+
+@Data
+public class AuthResponseBodyModel {
+    Boolean success;
+    UserData data;
+    String appVersion;
+
+    @Data
+    public static class UserData {
+        String id, apiToken;
+        Boolean newUser;
+        String username;
+    }
+}
