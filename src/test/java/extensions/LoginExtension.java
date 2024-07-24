ea65785cd9d831358ae06c41d71ea8ae0aef181d
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-24 11:01:49 +0300

    added LoginExtension.java

---
+++ src/test/java/extensions/LoginExtension.java
@@ -0,0 +1,12 @@
+package extensions;
+import authorization.Authorization;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class LoginExtension implements BeforeEachCallback {
+    @Override
+    public void beforeEach(ExtensionContext context) throws JsonProcessingException {
+        Authorization.setAuthDataInLocalStorage(Authorization.getAuthResponse());
+    }
+}
