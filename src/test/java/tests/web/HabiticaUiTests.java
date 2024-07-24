69e9785ec2f97505563f84dbfc877a52f92ce35f
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-24 16:12:38 +0300

    updated HabiticaUiTests

--- src/test/java/tests/web/HabiticaUiTests.java
+++ src/test/java/tests/web/HabiticaUiTests.java
@@ -1,10 +1,13 @@
-package tests;
+package tests.web;
 
 import extensions.WithLogin;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import tests.TestBase;
 
 import static com.codeborne.selenide.Selenide.open;
 
+@Tag("ui")
 public class HabiticaUiTests extends TestBase {
 
     @Test
