98a5ba17b1555baa4ef88ed638b4ac5772807d36
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-25 20:15:26 +0300

    updated HabiticaUiTests.java

--- src/test/java/tests/web/HabiticaUiTests.java
+++ src/test/java/tests/web/HabiticaUiTests.java
@@ -1,16 +1,42 @@
 package tests.web;
+
+import com.codeborne.selenide.Selenide;
 import extensions.WithLogin;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Selenide.open;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.restassured.RestAssured.given;
 
 @Tag("ui")
 public class HabiticaUiTests extends TestBase {
 
     @Test
     @WithLogin
-    void profileTest(){
+    void addTaskTest(){
         open("");
+        $(byTagAndText("div", "Добавить задачу")).click();
+        $(byTagAndText("div", "задачу")).click();
+        $(".task-purple-modal-input").setValue("Сделать диплом");
+        $(byTagAndText("button", "Создать")).click();
+        $(".task-content").shouldHave(text("Сделать диплом"));
+    }
+
+    @Test
+    @WithLogin
+    void deleteTaskTest(){
+        open("");
+        $(byTagAndText("div", "Добавить задачу")).click();
+        $(byTagAndText("div", "задачу")).click();
+        $(".task-purple-modal-input").setValue("Сходить в магазин");
+        $(byTagAndText("button", "Создать")).click();
+        $(byTagAndText("p", "Сходить в магазин")).click();
+        $(byTagAndText("span", "Удалить задачу")).click();
+        Selenide.switchTo().alert().accept();
+        $(".task-content").shouldNotHave(text("Сходить в магазин"));
     }
 
 }
