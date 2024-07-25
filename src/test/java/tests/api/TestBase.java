d01c626e19890e3b20acbd2c324be761c869c269
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-25 20:15:44 +0300

    updated TestBase.java

--- src/test/java/tests/api/TestBase.java
+++ src/test/java/tests/api/TestBase.java
@@ -1,14 +1,19 @@
 package tests.api;
 
+import config.ApiConfig;
+import config.WebConfig;
 import io.restassured.RestAssured;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
 
     @BeforeAll
     public static void beforeAll(){
-        RestAssured.baseURI = "https://habitica.com";
-        RestAssured.basePath = "/api";
+
+        ApiConfig apiConfig = ConfigFactory.create(ApiConfig.class, System.getProperties());
+        RestAssured.baseURI = apiConfig.baseURI();
+        RestAssured.basePath = apiConfig.basePath();
 
     }
 }
