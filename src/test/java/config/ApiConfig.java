3daf8d163b5dc4b57167cc24946f9b2f11d75a7d
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-27 01:46:33 +0300

    added ApiConfig.java

--- src/test/java/config/ApiConfig.java
+++ src/test/java/config/ApiConfig.java
@@ -9,9 +9,27 @@ import org.aeonbits.owner.Config;
 public interface ApiConfig extends Config {
 
     @DefaultValue("https://habitica.com")
+    String baseUrl();
+
+    @DefaultValue("chrome")
+    String browser();
+
+    @DefaultValue("1920x1080")
+    String browserSize();
+
+    @DefaultValue("126")
+    String browserVersion();
+
+    @DefaultValue("https://habitica.com")
     String baseURI();
 
     @DefaultValue("/api")
     String basePath();
+
+    @DefaultValue("false")
+    boolean isRemote();
+
+    @DefaultValue("https://habitica.com")
+    String remoteUrl();
 
 }
