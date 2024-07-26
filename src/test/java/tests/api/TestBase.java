f689ecb047c14663e4431a343ecbd810be4e064b
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-27 01:50:05 +0300

    updated TestBase.java

--- src/test/java/tests/api/TestBase.java
+++ src/test/java/tests/api/TestBase.java
@@ -1,5 +1,6 @@
 package tests.api;
 
+import com.codeborne.selenide.Configuration;
 import config.ApiConfig;
 import config.WebConfig;
 import io.restassured.RestAssured;
@@ -12,8 +13,17 @@ public class TestBase {
     public static void beforeAll(){
 
         ApiConfig apiConfig = ConfigFactory.create(ApiConfig.class, System.getProperties());
+        Configuration.baseUrl = apiConfig.baseUrl();
+        Configuration.browser = apiConfig.browser();
+        Configuration.browserSize = apiConfig.browserSize();
+        Configuration.browserVersion = apiConfig.browserVersion();
         RestAssured.baseURI = apiConfig.baseURI();
         RestAssured.basePath = apiConfig.basePath();
+        if(apiConfig.isRemote()){
+            Configuration.remote = apiConfig.remoteUrl();
+        }
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
 
     }
 }
