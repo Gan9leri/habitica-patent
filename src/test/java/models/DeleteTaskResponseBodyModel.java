a8f23835260299b1a98836e965f132f555ba0af7
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-27 01:47:53 +0300

    added DeleteTaskResponseBodyModel.java

---
+++ src/test/java/models/DeleteTaskResponseBodyModel.java
@@ -0,0 +1,23 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+@Data
+public class DeleteTaskResponseBodyModel {
+    Boolean success;
+    TaskData data;
+    List<UserNotifications> notifications;
+    int userV;
+    String appVersion;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class TaskData{
+
+    }
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class UserNotifications{
+
+    }
+}
