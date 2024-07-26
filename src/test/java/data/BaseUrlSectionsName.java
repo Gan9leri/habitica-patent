7c9aa5bff545d460c6db20fd3b75256ce37148d5
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-27 01:47:07 +0300

    added BaseUrlSectionsName.java

---
+++ src/test/java/data/BaseUrlSectionsName.java
@@ -0,0 +1,27 @@
+package data;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseUrlSectionsName {
+
+    ISSUES ("Задачи","https://habitica.com/"),
+    INVENTORY ("Инвентарь", "https://habitica.com/inventory/items"),
+    SHOP ("Лавки", "https://habitica.com/shops/market"),
+    TEAM ("Команда", "https://habitica.com/party"),
+    GROUP ("Группа", "https://habitica.com/group-plans"),
+    CHALLENGES ("Испытания", "https://habitica.com/challenges/myChallenges"),
+    FAQ ("Помощь", "https://habitica.com/static/faq");
+
+
+    private final String value;
+    private final String link;
+
+    BaseUrlSectionsName(String value, String link) {
+        this.value = value;
+        this.link = link;
+    }
+
+
+
+}
