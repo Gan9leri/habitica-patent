4d25ff504c64a6c15f8a007fb87c6348f1775dce
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-27 22:00:05 +0300

    updated TestBase.java

--- src/test/java/tests/mobile/TestBase.java
+++ src/test/java/tests/mobile/TestBase.java
@@ -4,19 +4,22 @@ import com.codeborne.selenide.Configuration;
 import drivers.BrowserstackDriver;
 import drivers.EmulationDriver;
 import drivers.RealDeviceDriver;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
+
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = null;
         Configuration.timeout = 30000;
         //Configuration.browser = BrowserstackDriver.class.getName();
         Configuration.browser = RealDeviceDriver.class.getName();
-        if ("browserstack".equals(System.getProperty("deviceHost"))) {
+       /* if ("browserstack".equals(System.getProperty("deviceHost"))) {
             Configuration.browser = BrowserstackDriver.class.getName();
         }
         else if ("real".equals(System.getProperty("deviceHost"))){
@@ -24,11 +27,16 @@ public class TestBase {
         }
         else if ("emulation".equals(System.getProperty("deviceHost"))){
             Configuration.browser = EmulationDriver.class.getName();
-        }
+        }*/
     }
 
     @BeforeEach
     void beforeEach() {
-        open();
+        //open();
+    }
+
+    @AfterEach
+    void afterEach() {
+        closeWebDriver();
     }
 }
