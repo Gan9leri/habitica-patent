d2274dbb6f51dca42b474daa1091b7c115418d40
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-27 19:28:23 +0300

    added HabiticaMobileTests.java

---
+++ src/test/java/tests/mobile/HabiticaMobileTests.java
@@ -0,0 +1,18 @@
+package tests.mobile;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static io.appium.java_client.AppiumBy.id;
+
+@Tag("mobile")
+public class HabiticaMobileTests extends TestBase{
+
+    @Test
+    void test(){
+        //$(id("com.habitrpg.android.habitica:id/skipButton")).click();
+
+    }
+
+}
