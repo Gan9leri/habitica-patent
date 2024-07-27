ac0615494c07cc4c6559f8624ff7e03c291a8249
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-27 21:59:02 +0300

    updated HabiticaMobileTests.java

--- src/test/java/tests/mobile/HabiticaMobileTests.java
+++ src/test/java/tests/mobile/HabiticaMobileTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static io.appium.java_client.AppiumBy.id;
 
 @Tag("mobile")
@@ -11,7 +12,9 @@ public class HabiticaMobileTests extends TestBase{
 
     @Test
     void test(){
-        //$(id("com.habitrpg.android.habitica:id/skipButton")).click();
+        open();
+        $(id("com.habitrpg.android.habitica:id/new_game_button")).click();
+
 
     }
 
