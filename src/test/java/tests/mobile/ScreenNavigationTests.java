bc195b879edb006113e86ea4affd4f2439d57bfc
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-29 00:11:02 +0300

    updated ScreenNavigationTests.java

--- src/test/java/tests/mobile/ScreenNavigationTests.java
+++ src/test/java/tests/mobile/ScreenNavigationTests.java
@@ -2,7 +2,6 @@ package tests.mobile;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
