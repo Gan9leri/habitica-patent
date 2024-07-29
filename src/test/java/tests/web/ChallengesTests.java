bf4b9fd2bfcedfafad3d7e6a2d8fb5cb9b0fd2ac
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-29 16:40:23 +0300

    added ChallengesTests.java

---
+++ src/test/java/tests/web/ChallengesTests.java
@@ -0,0 +1,29 @@
+package tests.web;
+
+import config.AuthConfig;
+import extensions.WithLogin;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.ChallengesPage;
+
+@DisplayName("Тесты для страницы испытаний")
+@Tag("ui")
+@Owner("Овсянников Александр")
+@Feature("Challenges")
+public class ChallengesTests extends TestBase {
+
+    ChallengesPage challengesPage = new ChallengesPage();
+    AuthConfig authConfig = ConfigFactory.create(AuthConfig.class, System.getProperties());
+
+    @DisplayName("Проверка владельца испытания")
+    @Test
+    @WithLogin
+    void ownerOfChallengeTest(){
+        challengesPage.openChallengePage()
+                        .checkOwnerOfChallenge(authConfig.login());
+    }
+}
