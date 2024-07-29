bb539b013dad2597a595eb7c8433c56cc3cc6df8
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-29 18:01:06 +0300

    added MainPage.java

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,4 +1,54 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class MainPage {
+
+    SelenideElement addTaskButton = $(byTagAndText("div", "Добавить задачу")),
+                    itemTask =  $(byTagAndText("div", "задачу")),
+                    inputArea =  $(".task-purple-modal-input"),
+                    createButton = $(byTagAndText("button", "Создать"));
+
+    @Step("Открытие главной страницы")
+    public MainPage openMainPage(){
+        open("");
+        return this;
+    }
+
+    @Step("Нажатие на кнопку Добавить задачу")
+    public MainPage addTaskClick(){
+        addTaskButton.click();
+        return this;
+    }
+
+    @Step("Выбор пункта из выпадающего списка")
+    public MainPage itemTaskSelection(){
+        itemTask.click();
+        return this;
+    }
+
+    @Step("Ввод названия задачи")
+    public MainPage inputTaskName(String value){
+        inputArea.setValue(value);
+        return this;
+    }
+
+    @Step("Нажатие на кнопку Создать")
+    public MainPage createButtonClick(){
+        createButton.click();
+        return this;
+    }
+
+    @Step("Проверка, что задача создалась")
+    public MainPage checkingTasksContainer(String value){
+        $(byTagAndText("p", value)).shouldHave(text(value));
+        return this;
+    }
+
 }
