1b052e028d0f60f2abfe12fb611a802314ceb543
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-29 18:02:10 +0300

    added TasksTests.java

--- src/test/java/tests/web/TasksTests.java
+++ src/test/java/tests/web/TasksTests.java
@@ -1,26 +1,39 @@
 package tests.web;
 
 import com.github.javafaker.Faker;
+import data.DataGeneration;
 import extensions.WithLogin;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import pages.MainPage;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-@Tag("ui")
+@DisplayName("Тесты для главной страницы")
+@Tag("web")
+@Owner("Овсянников Александр")
+@Feature("MainPage")
 public class TasksTests extends TestBase{
+
+    DataGeneration data = new DataGeneration();
+    MainPage mainPage = new MainPage();
+
+    @DisplayName("Проверка возможности создания задачи")
     @Test
     @WithLogin
     void addTaskTest(){
-        Faker faker = new Faker();
-        String task = faker.job().keySkills();
-        open("");
-        $(byTagAndText("div", "Добавить задачу")).click();
-        $(byTagAndText("div", "задачу")).click();
-        $(".task-purple-modal-input").setValue(task);
-        $(byTagAndText("button", "Создать")).click();
-        $(byTagAndText("p", task)).shouldHave(text(task));
+        String task = data.task;
+        mainPage.openMainPage()
+                .addTaskClick()
+                .itemTaskSelection()
+                .inputTaskName(task)
+                .createButtonClick()
+                .checkingTasksContainer(task);
     }
 }
