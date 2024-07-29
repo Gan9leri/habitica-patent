aa1c3b0f6645cc337f6c25eb89373ac61245d632
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-29 16:43:38 +0300

    added PartyTests.java

---
+++ src/test/java/tests/web/PartyTests.java
@@ -0,0 +1,31 @@
+package tests.web;
+
+import data.DataGeneration;
+import extensions.WithLogin;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.PartyPage;
+
+@DisplayName("Тесты для страницы команды")
+@Tag("ui")
+@Owner("Овсянников Александр")
+@Feature("Party")
+public class PartyTests extends TestBase{
+
+    DataGeneration data = new DataGeneration();
+    PartyPage partyPage = new PartyPage();
+
+    @DisplayName("Проверка отправки сообщения в чат команды")
+    @Test
+    @WithLogin
+    void sendingAMessageToTeamChatTest(){
+        String message = data.message;
+        partyPage.openPartyPage()
+                .enteringAMessageInChat(message)
+                .sendingAMessage()
+                .checkPartyChat(message);
+    }
+}
