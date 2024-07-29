2e5528f504536d817801931a6c8cdb66419c8901
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-29 21:32:35 +0300

    updated MainPage.java

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,19 +1,24 @@
 package pages;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byTagAndText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.currentFrameUrl;
 
 public class MainPage {
 
     SelenideElement addTaskButton = $(byTagAndText("div", "Добавить задачу")),
                     itemTask =  $(byTagAndText("div", "задачу")),
                     inputArea =  $(".task-purple-modal-input"),
-                    createButton = $(byTagAndText("button", "Создать"));
+                    createButton = $(byTagAndText("button", "Создать")),
+                    communityRulesButton = $(byTagAndText("a", "Правила сообщества")),
+                    communityRulestitle = $("#welcome");
+    ElementsCollection menuCollapse = $("#menu_collapse").$$("li");
 
     @Step("Открытие главной страницы")
     public MainPage openMainPage(){
@@ -48,6 +53,36 @@ public class MainPage {
     @Step("Проверка, что задача создалась")
     public MainPage checkingTasksContainer(String value){
         $(byTagAndText("p", value)).shouldHave(text(value));
+        return this;
+    }
+
+    @Step("Нажатие на кнопку Правила сообщества")
+    public MainPage cummunityRulesClick(){
+        communityRulesButton.click();
+        return this;
+    }
+
+    @Step("Переход к следующей вкладке")
+    public MainPage moveToNewTab(){
+        webdriver().driver().switchTo().window(1);
+        return this;
+    }
+
+    @Step("Проверка Url")
+    public MainPage checkUrl(String value){
+        webdriver().shouldHave(currentFrameUrl(value));
+        return this;
+    }
+
+    @Step("Проверка заголовка")
+    public MainPage checkCommunityRulesTitle(String value){
+        communityRulestitle.shouldHave(text(value));
+        return this;
+    }
+
+    @Step("Нажатие на кнопку раздела")
+    public MainPage menuCollapseElementClick(String value){
+        menuCollapse.filter(visible).find(text(value)).click();
         return this;
     }
 
