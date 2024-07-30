14cdc48b6c2b1a4be63b24351322d7061745e1b8
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-30 13:37:49 +0300

    updated ScreenNavigationTests.java

--- src/test/java/tests/mobile/ScreenNavigationTests.java
+++ src/test/java/tests/mobile/ScreenNavigationTests.java
@@ -1,50 +1,60 @@
 package tests.mobile;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static io.appium.java_client.AppiumBy.id;
+import screens.IntroScreen;
+import screens.LoginScreen;
+import screens.RegisterScreen;
+import screens.WelcomeScreen;
 
+
+@DisplayName("Тесты на навигацию по начальным экранам")
 @Tag("mobile")
+@Owner("Овсянников Александр")
+@Feature("LoginApi")
 public class ScreenNavigationTests extends TestBase{
 
+    IntroScreen introScreen = new IntroScreen();
+    WelcomeScreen welcomeScreen = new WelcomeScreen();
+    LoginScreen loginScreen = new LoginScreen();
+    RegisterScreen registerScreen = new RegisterScreen();
+
+    @DisplayName("Проверка возможности пропустить Intro")
     @Test
     void checkIntroScreenTest() throws InterruptedException {
 
-        $(id("com.habitrpg.android.habitica:id/skipButton")).click();
-        $(id("com.habitrpg.android.habitica:id/show_login_button")).shouldHave(text("LOGIN")).shouldBe(visible);
-        $(id("com.habitrpg.android.habitica:id/new_game_button")).shouldHave(text("REGISTER")).shouldBe(visible);
-
+        introScreen.skipIntro();
+        welcomeScreen.checkLoginButton("LOGIN")
+                     .checkRegisterButton("REGISTER");
     }
 
+    @DisplayName("Проверка возможности перейти к экрану авторизации")
     @Test
     void checkLoginScreenTest(){
 
-        $(id("com.habitrpg.android.habitica:id/skipButton")).click();
-        $(id("com.habitrpg.android.habitica:id/show_login_button")).click();
-        $(id("com.habitrpg.android.habitica:id/username")).shouldHave(text("Email/Username"));
-        $(id("com.habitrpg.android.habitica:id/password")).shouldHave(text("Password"));
-        $(id("com.habitrpg.android.habitica:id/back_button")).click();
-        $(id("com.habitrpg.android.habitica:id/show_login_button")).shouldHave(text("LOGIN")).shouldBe(visible);
-        $(id("com.habitrpg.android.habitica:id/new_game_button")).shouldHave(text("REGISTER")).shouldBe(visible);
-
+        introScreen.skipIntro();
+        welcomeScreen.loginButtonClick();
+        loginScreen.checkUsernameField("Email/Username")
+                   .checkPasswordField("Password")
+                   .backButtonClick();
+        welcomeScreen.checkLoginButton("LOGIN")
+                     .checkRegisterButton("REGISTER");
     }
 
+    @DisplayName("Проверка возможности перейти к экрану регистрации")
     @Test
     void checkRegisterScreenTest(){
-
-        $(id("com.habitrpg.android.habitica:id/skipButton")).click();
-        $(id("com.habitrpg.android.habitica:id/new_game_button")).click();
-        $(id("com.habitrpg.android.habitica:id/username")).shouldHave(text("Username"));
-        $(id("com.habitrpg.android.habitica:id/email")).shouldHave(text("Email address"));
-        $(id("com.habitrpg.android.habitica:id/password")).shouldHave(text("Password"));
-        $(id("com.habitrpg.android.habitica:id/confirm_password")).shouldHave(text("Confirm password"));
-        $(id("com.habitrpg.android.habitica:id/back_button")).click();
-        $(id("com.habitrpg.android.habitica:id/show_login_button")).shouldHave(text("LOGIN")).shouldBe(visible);
-        $(id("com.habitrpg.android.habitica:id/new_game_button")).shouldHave(text("REGISTER")).shouldBe(visible);
-
+        introScreen.skipIntro();
+        welcomeScreen.newGameButtonClick();
+        registerScreen.checkUsernameField("Username")
+                      .checkEmailField("Email address")
+                      .checkPasswordField("Password")
+                      .checkConformPasswordField("Confirm password")
+                      .backButtonClick();
+        welcomeScreen.checkLoginButton("LOGIN")
+                .checkRegisterButton("REGISTER");
     }
-
 }
