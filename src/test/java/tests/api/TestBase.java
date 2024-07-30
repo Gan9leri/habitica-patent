2adf0cda681a1834cbebc050c5199035c70f8bbf
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-30 13:39:13 +0300

    updated TestBase.java

--- src/test/java/tests/api/TestBase.java
+++ src/test/java/tests/api/TestBase.java
@@ -1,17 +1,23 @@
 package tests.api;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import config.ApiConfig;
 import config.WebConfig;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
     @BeforeAll
     public static void beforeAll(){
-
+        SelenideLogger.addListener("allure", new AllureSelenide());
         ApiConfig apiConfig = ConfigFactory.create(ApiConfig.class, System.getProperties());
         Configuration.baseUrl = apiConfig.baseUrl();
         Configuration.browser = apiConfig.browser();
@@ -25,5 +31,11 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
 
+    }
+
+    @AfterEach
+    void addAttachments() {
+
+        closeWebDriver();
     }
 }
