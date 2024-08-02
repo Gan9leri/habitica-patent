561a4c8dcfae02f2ecb220d76f56247ab137ec5c
AOvsyannikov <aovsyannikov@it-one.ru>
2024-08-02 17:37:01 +0300

    added TaskTests.java

--- src/test/java/tests/api/TaskTests.java
+++ src/test/java/tests/api/TaskTests.java
@@ -1,22 +1,21 @@
 package tests.api;
 
-import extensions.WithLogin;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import data.DataGeneration;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
-import models.DeleteTaskResponseBodyModel;
-import models.UserResponseBodyModel;
+import models.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.IOException;
 import static authorization.Authorization.getAuthResponse;
-//import static com.codeborne.selenide.logevents.SelenideLogger.step;
+import static extensions.TaskExtension.*;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
-import static specs.Specs.RequestSpec;
-import static specs.Specs.ResponseSpec200;
-import static extensions.TaskExtension.addTask;
-import static extensions.TaskExtension.getTaskId;
+import static specs.Specs.*;
 
 @DisplayName("Тесты на работу с задачами")
 @Tag("api")
@@ -24,51 +23,51 @@ import static extensions.TaskExtension.getTaskId;
 @Feature("LoginApi")
 public class TaskTests extends TestBase{
 
-    @DisplayName("Тест на получение информации по задаче")
-    @Test
-    @WithLogin
-    void getTaskTest(){
-
-        String task = addTask();
-        String xApiUser = getAuthResponse().getData().getId();
-        String xApiKey = getAuthResponse().getData().getApiToken();
-        UserResponseBodyModel response =
-                step("Отправка GET запроса на получение информации по задачам", () ->
-                    given(RequestSpec)
-                        .header("X-Api-User", xApiUser)
-                        .header("X-Api-Key", xApiKey)
-                        .when()
-                        .get("v4/tasks/user")
-                        .then()
-                        .spec(ResponseSpec200)
-                        .extract().as(UserResponseBodyModel.class)
-                );
-        step("Проверка названия задачи", () ->
-            assertThat(response.getData().get(0).getText()).isEqualTo(task)
-        );
-    }
-
     @DisplayName("Тест на удаление задачи")
     @Test
-    @WithLogin
-    void deleteTest(){
-
-        String taskId = getTaskId();
-        String xApiUser = getAuthResponse().getData().getId();
-        String xApiKey = getAuthResponse().getData().getApiToken();
+    void deleteTest() throws IOException {
+        AuthResponseBodyModel authResponse = getAuthResponse();
+        String taskId = createTask();
+        System.out.println(taskId);
         DeleteTaskResponseBodyModel response =
                 step("Отправка DELETE запроса на удаление задачи", () ->
                     given(RequestSpec)
-                        .header("X-Api-User", xApiUser)
-                        .header("X-Api-Key", xApiKey)
-                        .when()
-                        .delete("v4/tasks/" + taskId)
-                        .then()
-                        .spec(ResponseSpec200)
-                        .extract().as(DeleteTaskResponseBodyModel.class)
+                            .header("X-Api-Key", getAuthResponse().getData().getApiToken())
+                            .header("X-Api-User", getAuthResponse().getData().getId())
+                            .when()
+                            .delete("v4/tasks/" + taskId)
+                            .then()
+                            .spec(ResponseSpec200)
+                            .extract().as(DeleteTaskResponseBodyModel.class)
                 );
         step("Проверка успешности удаления задачи", () ->
             assertThat(response.getSuccess()).isEqualTo(true)
         );
+    }
+
+
+    @Test
+    void addApiTask() throws IOException {
+        DataGeneration data = new DataGeneration();
+        String text = data.task;
+        String id = data.taskId;
+        ObjectMapper om = new ObjectMapper();
+        PostCreateTaskUserRequestModel[] request = om.readValue(
+                new File("src/test/resources/request/PostAddTask.json"),
+                PostCreateTaskUserRequestModel[].class
+        );
+        request[0].setText(text);
+        request[0].set_id(id);
+        PostCreateTaskUserResponseModel response =
+        given(RequestSpec)
+                .when()
+                .body(request)
+                .header("X-Api-Key", getAuthResponse().getData().getApiToken())
+                .header("X-Api-User", getAuthResponse().getData().getId())
+                .post("/v4/tasks/user")
+                .then()
+                .spec(responseSpec201)
+                .extract().as(PostCreateTaskUserResponseModel.class);
+        assertThat(response.getSuccess()).isEqualTo(true);
     }
 }
