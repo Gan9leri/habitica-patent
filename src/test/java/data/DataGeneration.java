76474dea1c8e3e143d9f60e66cf8f25012400c3b
AOvsyannikov <aovsyannikov@it-one.ru>
2024-08-02 17:33:30 +0300

    updated DataGeneration.java

--- src/test/java/data/DataGeneration.java
+++ src/test/java/data/DataGeneration.java
@@ -1,10 +1,14 @@
 package data;
 import com.github.javafaker.Faker;
 
+import java.util.UUID;
+
 public class DataGeneration {
 
     Faker faker = new Faker();
     public String message = faker.name().firstName();
     public String task = faker.job().keySkills();
+    public String taskName = faker.funnyName().name();
+    public String taskId = UUID.randomUUID().toString();
 
 }
