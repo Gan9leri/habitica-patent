569546aad787a2434109cbbf2a76f392e6642664
AOvsyannikov <aovsyannikov@it-one.ru>
2024-08-02 17:36:40 +0300

    added TaskExtension.java

--- src/test/java/extensions/TaskExtension.java
+++ src/test/java/extensions/TaskExtension.java
@@ -1,48 +1,45 @@
 package extensions;
 
-import com.github.javafaker.Faker;
-import io.restassured.http.ContentType;
-import models.UserResponseBodyModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import data.DataGeneration;
+import models.AuthResponseBodyModel;
+import models.PostCreateTaskUserRequestModel;
+import models.PostCreateTaskUserResponseModel;
+
+import java.io.File;
+import java.io.IOException;
+
 import static authorization.Authorization.getAuthResponse;
-import static com.codeborne.selenide.Selectors.byTagAndText;
-import static com.codeborne.selenide.Selenide.*;
 import static io.restassured.RestAssured.given;
+import static specs.Specs.RequestSpec;
+import static specs.Specs.responseSpec201;
 
 public class TaskExtension {
 
-    public static String addTask(){
-        Faker faker = new Faker();
-        String task = faker.job().keySkills();
-        open("");
-        $(byTagAndText("div", "Добавить задачу")).click();
-        $(byTagAndText("div", "задачу")).click();
-        $(".task-purple-modal-input").setValue(task);
-        $(byTagAndText("button", "Создать")).click();
-        closeWindow();
-        return task;
-    }
-
-    public static String getTaskId(){
-        Faker faker = new Faker();
-        String task = faker.job().keySkills();
-        open("");
-        $(byTagAndText("div", "Добавить задачу")).click();
-        $(byTagAndText("div", "задачу")).click();
-        $(".task-purple-modal-input").setValue(task);
-        $(byTagAndText("button", "Создать")).click();
-        String xApiUser = getAuthResponse().getData().getId();
-        String xApiKey = getAuthResponse().getData().getApiToken();
-        UserResponseBodyModel response =
-                given().log().all()
-                        .contentType(ContentType.JSON)
-                        .header("X-Api-User", xApiUser)
-                        .header("X-Api-Key", xApiKey)
+    public static String createTask() throws IOException {
+        DataGeneration data = new DataGeneration();
+        AuthResponseBodyModel authResponse = getAuthResponse();
+        String text = data.task;
+        String id = data.taskId;
+        ObjectMapper om = new ObjectMapper();
+        PostCreateTaskUserRequestModel[] request = om.readValue(
+                new File("src/test/resources/request/PostAddTask.json"),
+                PostCreateTaskUserRequestModel[].class
+        );
+        request[0].setText(text);
+        request[0].set_id(id);
+        PostCreateTaskUserResponseModel response =
+                given(RequestSpec)
                         .when()
-                        .get("v4/tasks/user")
-                        .then().log().all()
-                        .statusCode(200)
-                        .extract().as(UserResponseBodyModel.class);
-        closeWindow();
-        return response.getData().get(0).getId();
+                        .body(request)
+                        .header("X-Api-Key", authResponse.getData().getApiToken())
+                        .header("X-Api-User", authResponse.getData().getId())
+                        .post("/v4/tasks/user")
+                        .then()
+                        .spec(responseSpec201)
+                        .extract().as(PostCreateTaskUserResponseModel.class);
+        return response.getData().get_id();
     }
 }
+
+
