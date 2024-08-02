1b2c25319e1ccc5fcbd5b051ea5e7c25855595f7
AOvsyannikov <aovsyannikov@it-one.ru>
2024-08-02 17:34:55 +0300

    added PostCreateTaskUserResponseModel.java

---
+++ src/test/java/models/PostCreateTaskUserResponseModel.java
@@ -0,0 +1,19 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PostCreateTaskUserResponseModel {
+    private Boolean success;
+    private Data data;
+
+    @lombok.Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Data {
+        private String type;
+        private String text;
+        private String _id;
+    }
+}
