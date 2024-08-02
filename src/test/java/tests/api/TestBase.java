40a490082f8151677d6fc129f89417c8ed90c9fe
AOvsyannikov <aovsyannikov@it-one.ru>
2024-08-02 17:37:26 +0300

    updated TestBase.java

--- src/test/java/tests/api/TestBase.java
+++ src/test/java/tests/api/TestBase.java
@@ -19,23 +19,12 @@ public class TestBase {
     public static void beforeAll(){
         SelenideLogger.addListener("allure", new AllureSelenide());
         ApiConfig apiConfig = ConfigFactory.create(ApiConfig.class, System.getProperties());
-        Configuration.baseUrl = apiConfig.baseUrl();
-        Configuration.browser = apiConfig.browser();
-        Configuration.browserSize = apiConfig.browserSize();
-        Configuration.browserVersion = apiConfig.browserVersion();
         RestAssured.baseURI = apiConfig.baseURI();
         RestAssured.basePath = apiConfig.basePath();
-        if(apiConfig.isRemote()){
-            Configuration.remote = apiConfig.remoteUrl();
-        }
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
-
     }
 
     @AfterEach
     void addAttachments() {
-
         closeWebDriver();
     }
 }
