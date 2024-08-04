43d889fc42fdb242e42365d83c66b01f77236f95
AOvsyannikov <aovsyannikov@it-one.ru>
2024-08-04 16:37:20 +0300

    refactoring for Jenkins

--- src/test/java/screens/IntroScreen.java
+++ src/test/java/screens/IntroScreen.java
@@ -3,6 +3,7 @@ package screens;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.Selenide.$;
 import static io.appium.java_client.AppiumBy.id;
 
 public class IntroScreen {
--- src/test/java/screens/WelcomeScreen.java
+++ src/test/java/screens/WelcomeScreen.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
 import static io.appium.java_client.AppiumBy.id;
 
 public class WelcomeScreen {
